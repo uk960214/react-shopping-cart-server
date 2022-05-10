997493b310fbf297b10c5437a6dfd04bda2cd340
Yo Wook Kim <uk960214@gmail.com>
2022-05-10 16:22:19 +0900

    feat: 서버 설정

---
+++ server.js
@@ -0,0 +1,23 @@
+const path = require("path");
+const express = require("express");
+const jsonServer = require("json-server");
+const data = require("./data.json");
+
+const router = jsonServer.router(data); // Express router
+const server = jsonServer.create(); // Express server
+
+server.use("/static", express.static(path.join(__dirname, "public")));
+
+// Avoid CORS issue
+server.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
+server.use(router);
+
+server.listen(3000);
